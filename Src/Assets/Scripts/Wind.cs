9beb5aeaf662536d37357d5b7f259a31bb4ab533
jose de jesus <josejoeldejesus@gmail.com>
2015-01-24 07:17:04 -0430

    Working on wind

---
+++ Src/Assets/Scripts/Wind.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+	public Vector3 windDirection;
+	public bool wind= true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag== "Player") {
+			Debug.Log("WIND");
+			(other.gameObject.GetComponent("CharacterController") as CharacterController).Move(windDirection);
+		}
+	}
+}
