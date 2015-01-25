5956adff9954c12b5543250ccd33c377fd0f8c77
Ismael Serrada <ismael.serrada@gmail.com>
2015-01-24 19:37:02 -0430

    Update, on door prefab for survive scene

--- Src/Assets/Scripts/Enemy/EnemiesKilled.cs
+++ Src/Assets/Scripts/Enemy/EnemiesKilled.cs
@@ -2,10 +2,40 @@
 using System.Collections;
 
 public class EnemiesKilled : MonoBehaviour {
+	public int totalLevelEnemies;
+	public GameObject levelDoor;
+
 	public static int enemiesKilled;
+
+	bool  activated;
 
 	void Awake()
 	{
-		enemiesKilled = 0;
+//		enemiesKilled = 0;
+		levelDoor = GameObject.FindGameObjectWithTag ("Door");
+	}
+
+	void OnEnable()
+	{
+		Messenger<int>.AddListener("enemyKilled", SumEnemy);
+	}
+	
+	void OnDisable()
+	{
+		Messenger<int>.RemoveListener("enemyKilled", SumEnemy);
+	}
+	
+	void SumEnemy(int windDirection)
+	{
+
+	}
+
+	void Update()
+	{
+		Debug.Log ("Enemies Killed: "+ enemiesKilled);
+		if (enemiesKilled >= totalLevelEnemies) 
+		{
+			levelDoor.SetActive(true);
+		}
 	}
 }
