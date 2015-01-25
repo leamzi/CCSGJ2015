cfa410721380ce88884b31502e4725fccc70b74c
Ismael Serrada <ismael.serrada@gmail.com>
2015-01-25 14:44:18 -0430

    nuevo flow de pantalla inicial

--- Src/Assets/Scripts/Util/UISceneJumper.cs
+++ Src/Assets/Scripts/Util/UISceneJumper.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class UISceneJumper : MonoBehaviour {
 
 	public bool homeScene;
+	public bool controllerScene;
 
 	public void GoHome(){
 		Application.LoadLevel("Scene_Home");
@@ -21,11 +22,20 @@ public class UISceneJumper : MonoBehaviour {
 		Application.LoadLevel("Scene_credits");
 	}
 
+	public void GoControllers(){
+		Application.LoadLevel("Scene_controllers");
+	}
+
 	public void GetUIKeyBoard() {
 		// only in home button
 		if (this.homeScene) {
-			if(Input.GetButtonDown("btn_a_01") || Input.GetButtonDown("btn_a_02")) GoStart();
-		} else {
+			if(Input.GetButtonDown("btn_a_01") || Input.GetButtonDown("btn_a_02")) GoControllers();
+		} else if (this.controllerScene)
+		{
+			if(Input.GetButtonDown("btn_a_01") || Input.GetButtonDown("btn_a_02")) Application.LoadLevel("Scene_Wind");
+		}
+		else
+		{
 			if(Input.GetButtonDown("btn_b_01") || Input.GetButtonDown("btn_b_02")) GoHome();
 		}
 	}
