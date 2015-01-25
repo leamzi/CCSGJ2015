02c597c5ced3b1ebae5ba856dcd53184dfac3359
jose de jesus <josejoeldejesus@gmail.com>
2015-01-25 02:22:58 -0430

    Working in GUI for home, controllers and credits

---
+++ Src/Assets/Scripts/Util/UISceneJumper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISceneJumper : MonoBehaviour {
+
+	public bool homeScene;
+
+	public void GoHome(){
+		Application.LoadLevel("Scene_Home");
+	}
+
+	public void GoStart(){
+		Application.LoadLevel("Scene_Wind");
+	}
+
+	public void GoControls(){
+		Application.LoadLevel("Scene_controllers");
+	}
+
+	public void GoCredits(){
+		Application.LoadLevel("Scene_credits");
+	}
+
+	public void GetUIKeyBoard() {
+		// only in home button
+		if (this.homeScene) {
+			if(Input.GetButtonDown("btn_a_01")) GoStart();
+		} else {
+			if(Input.anyKeyDown) GoHome();
+		}
+	}
+
+	void Update() {
+		GetUIKeyBoard();
+	}
+}
